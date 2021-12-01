62b68d04890101fcfc597066785a265e7948941c
AxonBlAzE <s.mulkutkar@somaiya.edu>
2021-12-01 13:32:36 +0530

    minor imporvements

--- alt.py
+++ alt.py
@@ -12,41 +12,24 @@ def get_x():
         get_x()
     return x
 
-def state_diagram(states,inputs):
-    print("\nThe Final State Diagram for the above Transactions are as follows: ")
-    print("(0)",end="")
-    for (state,input) in zip(states,inputs):
-        print(" --"+str(input)+"--> ("+ str(state) + ")",end="")
-
-states = []
-inputs = []
 dfa = 0
 def balance(dfa,x):
     if dfa == 0:
         if x == 1:
             dfa = 5
-            states.append(dfa)
-            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 20
-            states.append(dfa)
-            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
-            states.append(dfa)
-            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
-            states.append(dfa)
-            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
-            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -56,28 +39,19 @@ def balance(dfa,x):
     elif dfa == 5:
         if x == 1:
             dfa = 10
-            states.append(dfa)
-            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 25
-            states.append(dfa)
-            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
-            states.append(dfa)
-            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
-            states.append(dfa)
-            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
-            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -87,28 +61,19 @@ def balance(dfa,x):
     elif dfa == 10:
         if x == 1:
             dfa = 15
-            states.append(dfa)
-            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 30
-            states.append(dfa)
-            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
-            states.append(dfa)
-            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
-            states.append(dfa)
-            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
-            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -118,28 +83,19 @@ def balance(dfa,x):
     elif dfa == 15:
         if x == 1:
             dfa = 20
-            states.append(dfa)
-            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 35
-            states.append(dfa)
-            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
-            states.append(dfa)
-            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
-            states.append(dfa)
-            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
-            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -149,28 +105,19 @@ def balance(dfa,x):
     elif dfa == 20:
         if x == 1:
             dfa = 25
-            states.append(dfa)
-            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 40
-            states.append(dfa)
-            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
-            states.append(dfa)
-            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
-            states.append(dfa)
-            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
-            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -180,23 +127,16 @@ def balance(dfa,x):
     elif dfa == 25:
         if x == 1 or x == 2:
             dfa = 25
-            states.append(dfa)
-            inputs.append("5,20")
             print("\nCannot add any further amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
             dfa = dfa - 25
-            states.append(dfa)
-            inputs.append('Buy')
             print("\nYou have bought a gumball")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
-            states.append(dfa)
-            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
-            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -206,23 +146,16 @@ def balance(dfa,x):
     elif dfa == 30:
         if x == 1 or x == 2:
             dfa = 30
-            states.append(dfa)
-            inputs.append("5,20")
             print("\nCannot add any further amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
             dfa = dfa - 25
-            states.append(dfa)
-            inputs.append('Buy')
             print("\nYou have bought a gumball")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
-            states.append(dfa)
-            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
-            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -232,23 +165,16 @@ def balance(dfa,x):
     elif dfa == 35:
         if x == 1 or x == 2:
             dfa = 35
-            states.append(dfa)
-            inputs.append("5,20")
             print("\nCannot add any further amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
             dfa = dfa - 25
-            states.append(dfa)
-            inputs.append('Buy')
             print("\nYou have bought a gumball")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
-            states.append(dfa)
-            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
-            state_diagram(states,inputs)
             return dfa
             
         show_menu()
@@ -258,23 +184,16 @@ def balance(dfa,x):
     elif dfa == 40:
         if x == 1 or x == 2:
             dfa = 40
-            states.append(dfa)
-            inputs.append("5,20")
             print("\nCannot add any further amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
             dfa = dfa - 25
-            states.append(dfa)
-            inputs.append('Buy')
             print("\nYou have bought a gumball")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
-            states.append(dfa)
-            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
-            state_diagram(states,inputs)
             return dfa
         
         show_menu()
--- gumball.py
+++ gumball.py
@@ -12,24 +12,41 @@ def get_x():
         get_x()
     return x
 
+def state_diagram(states,inputs):
+    print("\nThe Final State Diagram for the above Transactions are as follows: ")
+    print("(0)",end="")
+    for (state,input) in zip(states,inputs):
+        print(" --"+str(input)+"--> ("+ str(state) + ")",end="")
+
+states = []
+inputs = []
 dfa = 0
 def balance(dfa,x):
     if dfa == 0:
         if x == 1:
             dfa = 5
+            states.append(dfa)
+            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 20
+            states.append(dfa)
+            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
+            states.append(dfa)
+            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
+            states.append(dfa)
+            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
+            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -39,19 +56,28 @@ def balance(dfa,x):
     elif dfa == 5:
         if x == 1:
             dfa = 10
+            states.append(dfa)
+            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 25
+            states.append(dfa)
+            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
+            states.append(dfa)
+            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
+            states.append(dfa)
+            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
+            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -61,19 +87,28 @@ def balance(dfa,x):
     elif dfa == 10:
         if x == 1:
             dfa = 15
+            states.append(dfa)
+            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 30
+            states.append(dfa)
+            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
+            states.append(dfa)
+            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
+            states.append(dfa)
+            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
+            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -83,19 +118,28 @@ def balance(dfa,x):
     elif dfa == 15:
         if x == 1:
             dfa = 20
+            states.append(dfa)
+            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 35
+            states.append(dfa)
+            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
+            states.append(dfa)
+            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
+            states.append(dfa)
+            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
+            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -105,19 +149,28 @@ def balance(dfa,x):
     elif dfa == 20:
         if x == 1:
             dfa = 25
+            states.append(dfa)
+            inputs.append("5")
             print("\nYou have inserted ₹5")
             print("Current balance is ₹" + str(dfa))
         elif x == 2:
             dfa = 40
+            states.append(dfa)
+            inputs.append("20")
             print("\nYou have inserted ₹20")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
+            states.append(dfa)
+            inputs.append('Buy')
             print("Insufficient amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
+            states.append(dfa)
+            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
+            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -127,16 +180,23 @@ def balance(dfa,x):
     elif dfa == 25:
         if x == 1 or x == 2:
             dfa = 25
+            states.append(dfa)
+            inputs.append("5,20")
             print("\nCannot add any further amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
             dfa = dfa - 25
+            states.append(dfa)
+            inputs.append('Buy')
             print("\nYou have bought a gumball")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
+            states.append(dfa)
+            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
+            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -146,16 +206,23 @@ def balance(dfa,x):
     elif dfa == 30:
         if x == 1 or x == 2:
             dfa = 30
+            states.append(dfa)
+            inputs.append("5,20")
             print("\nCannot add any further amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
             dfa = dfa - 25
+            states.append(dfa)
+            inputs.append('Buy')
             print("\nYou have bought a gumball")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
+            states.append(dfa)
+            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
+            state_diagram(states,inputs)
             return dfa
 
         show_menu()
@@ -165,16 +232,23 @@ def balance(dfa,x):
     elif dfa == 35:
         if x == 1 or x == 2:
             dfa = 35
+            states.append(dfa)
+            inputs.append("5,20")
             print("\nCannot add any further amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
             dfa = dfa - 25
+            states.append(dfa)
+            inputs.append('Buy')
             print("\nYou have bought a gumball")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
+            states.append(dfa)
+            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
+            state_diagram(states,inputs)
             return dfa
             
         show_menu()
@@ -184,16 +258,23 @@ def balance(dfa,x):
     elif dfa == 40:
         if x == 1 or x == 2:
             dfa = 40
+            states.append(dfa)
+            inputs.append("5,20")
             print("\nCannot add any further amount")
             print("Current balance is ₹" + str(dfa))
         elif x == 3:
             dfa = dfa - 25
+            states.append(dfa)
+            inputs.append('Buy')
             print("\nYou have bought a gumball")
             print("Current balance is ₹" + str(dfa))
         elif x == 4:
             print("\nYou have withdrawn ₹"+str(dfa))
             dfa = 0
+            states.append(dfa)
+            inputs.append('Withdrawn')
             print("Current balance is ₹" + str(dfa))
+            state_diagram(states,inputs)
             return dfa
         
         show_menu()
@@ -204,10 +285,13 @@ def balance(dfa,x):
         print("\nSorry Something Went Wrong")
 
 print("\nWelcome to Gumball Machine")
-print("\nSelect an option")
-print("1. Insert ₹5")
-print("2. Insert ₹20")
-print("3. Buy Gumball")
-print("4. Withdraw money")
+print("Enter ₹25 to buy a gumball")
+print("\nCurrent balance is ₹0")
+# print("\nSelect an option")
+# print("1. Insert ₹5")
+# print("2. Insert ₹20")
+# print("3. Buy Gumball")
+# print("4. Withdraw money")
+show_menu()
 x = get_x()
 balance(dfa,x)
